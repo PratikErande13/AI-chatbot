10aa9285a5fab5d731ff23790bf14d5fe3af3d09
Pratik Erande <pratikerande31@gmail.com>
2025-04-23 20:32:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDORCsXWa_kIi7rFjIWWxz1eyA-AI1sIcI"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$GOOGLE GEMINI"
 let user={
     message:null,
     file:{
